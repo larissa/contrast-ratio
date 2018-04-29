cbe99e6a37ba389aa613d59b7e3b860862a6e765
Larissa Reis <larissa@colivre.coop.br>
2018-04-29 02:27:24 -0600

    Add a Panel Menu

--- extension.js
+++ extension.js
@@ -1,53 +1,69 @@
+/*
+This file is part of 'contrast-ratio-extension'.
+
+'contrast-ratio-extension' is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License (version 3) as
+published by the Free Software Foundation
+
+'contrast-ratio-extension' is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License
+along with 'contrast-ratio-extension'.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) 2018 Larissa Reis <reiss.larissa@gmail.com>
+*/
+
 
 const St = imports.gi.St;
 const Main = imports.ui.main;
-const Tweener = imports.ui.tweener;
+const Lang = imports.lang;
 
-let text, button;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
 
-function _hideHello() {
-    Main.uiGroup.remove_actor(text);
-    text = null;
-}
 
-function _showHello() {
-    if (!text) {
-        text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
-        Main.uiGroup.add_actor(text);
+let contrast_ratio_panel;
+
+const ContrastRatioPanel = new Lang.Class({
+    Name: 'ContrastRatioPanel',
+    Extends: PanelMenu.Button,
+    text: null,
+
+    _init: function() {
+       this.parent(0.0, "Contrast Ratio", false);
+
+       let icon = new St.Icon({
+           icon_name: 'color-select-symbolic',
+           style_class: 'system-status-icon'
+       });
+       this.actor.add_actor(icon);
+
+       let swap_colors_button = new PopupMenu.PopupMenuItem('Swap colors');
+       swap_colors_button.connect('activate', Lang.bind(this, this._onSwapColors));
+       this.menu.addMenuItem(swap_colors_button);
+    },
+
+    _onSwapColors: function() {
+        global.log("Swapping colors");
     }
-
-    text.opacity = 255;
-
-    let monitor = Main.layoutManager.primaryMonitor;
-
-    text.set_position(monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-                      monitor.y + Math.floor(monitor.height / 2 - text.height / 2));
-
-    Tweener.addTween(text,
-                     { opacity: 0,
-                       time: 2,
-                       transition: 'easeOutQuad',
-                       onComplete: _hideHello });
-}
+});
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'color-select-symbolic',
-                             style_class: 'system-status-icon' });
-
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
 }
 
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    contrast_ratio_panel = new ContrastRatioPanel();
+    Main.panel.addToStatusArea("contrast_ratio", contrast_ratio_panel, 0, "right");
 }
 
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    if (contrast_ratio_panel) {
+      Main.panel._rightBox.remove_actor(contrast_ratio_panel.container);
+      Main.panel.menuManager.removeMenu(contrast_ratio_panel.menu);
+      contrast_ratio_panel.destroy();
+      contrast_ratio_panel = null;
+    };
 }
