867e40adc068447e0a2d955e9992aedb9cdbffce
Larissa Reis <larissa@colivre.coop.br>
2018-05-12 16:43:29 -0600

    Use camelCase for variable names

--- extension.js
+++ extension.js
@@ -32,7 +32,7 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
 
-let contrast_ratio_panel;
+let contrastRatioPanel;
 
 const ColorInput = new Lang.Class({
     Name: 'ColorInput',
@@ -41,7 +41,7 @@ const ColorInput = new Lang.Class({
     _type: null,
     _colorEntry: null,
     _colorPreview: null,
-    _output_reader: null,
+    _outputReader: null,
     _panel: null,
 
     _init: function(type, color, parentMenu) {
@@ -69,18 +69,18 @@ const ColorInput = new Lang.Class({
 
     _onClickPreview: function() {
         // We only pick a color if we have an external tool available
-        let picker_path = GLib.spawn_command_line_sync('which grabc')[1].
+        let pickerPath = GLib.spawn_command_line_sync('which grabc')[1].
             toString().
             replace('\n', '');
 
-        if (picker_path !== '') {
+        if (pickerPath !== '') {
             this._getTopMenu().toggle();
             Mainloop.timeout_add(200, Lang.bind(this, function() {
-                let [res, pid, in_fd, out_fd, err_fd] = GLib.spawn_async_with_pipes(null, [picker_path], null, 0, null);
-                this._output_reader = new Gio.DataInputStream({
+                let [res, pid, in_fd, out_fd, err_fd] = GLib.spawn_async_with_pipes(null, [pickerPath], null, 0, null);
+                this._outputReader = new Gio.DataInputStream({
                     base_stream: new Gio.UnixInputStream({fd: out_fd})
                 });
-                this._output_reader.read_upto_async("", 0, 0, null, Lang.bind(this, this._getColorCallBack));
+                this._outputReader.read_upto_async("", 0, 0, null, Lang.bind(this, this._getColorCallBack));
             }));
         };
     },
@@ -90,7 +90,7 @@ const ColorInput = new Lang.Class({
     },
 
     _getColorCallBack: function(source_object, res) {
-        let [color, length] = this._output_reader.read_upto_finish(res);
+        let [color, length] = this._outputReader.read_upto_finish(res);
         this.update(color);
         this._panel.updateContrastRatio();
         Mainloop.timeout_add(200, Lang.bind(this, function() {
@@ -98,18 +98,18 @@ const ColorInput = new Lang.Class({
         }));
     },
 
-    _set_color: function(color) {
-        let res, rgba_color;
-        [res, rgba_color] = Clutter.Color.from_string(color);
+    _setColor: function(color) {
+        let res, rgbaColor;
+        [res, rgbaColor] = Clutter.Color.from_string(color);
         if (res) {
             // Remove alpha hex and normalize color to upper case
-            this.color = rgba_color.to_string().substr(0, 7).toUpperCase();
+            this.color = rgbaColor.to_string().substr(0, 7).toUpperCase();
             this._colorEntry.set_text(this.color);
         };
     },
 
     update: function(color) {
-        this._set_color(color);
+        this._setColor(color);
         this._updatePreviewColor();
     }
 
@@ -118,22 +118,22 @@ const ColorInput = new Lang.Class({
 const ContrastRatio = new Lang.Class({
     Name: 'ContrastRatio',
     Extends: PopupMenu.PopupBaseMenuItem,
-    _ratio_label: null,
-    _score_label: null,
+    _ratioLabel: null,
+    _scoreLabel: null,
 
-    _init: function(background_color, text_color) {
+    _init: function(backgroundColor, textColor) {
         this.parent({ reactive: false, style_class: 'contrast-ratio' });
 
-        this._ratio_label = new St.Label()
-        this._score_label = new St.Label()
+        this._ratioLabel = new St.Label()
+        this._scoreLabel = new St.Label()
 
-        this.actor.add(this._score_label, { expand: true, x_align: St.Align.START });
-        this.actor.add(this._ratio_label, { expand: true, x_align: St.Align.END });
+        this.actor.add(this._scoreLabel, { expand: true, x_align: St.Align.START });
+        this.actor.add(this._ratioLabel, { expand: true, x_align: St.Align.END });
 
-        this.update(background_color, text_color);
+        this.update(backgroundColor, textColor);
     },
 
-    _calculateContrastRatio: function(background_color, text_color) {
+    _calculateContrastRatio: function(backgroundColor, textColor) {
         // TODO: actually calculate contrast ratio
         return 5;
     },
@@ -162,23 +162,23 @@ const ContrastRatio = new Lang.Class({
         });
     },
 
-    update: function(background_color, text_color) {
-        let ratio = this._calculateContrastRatio(background_color, text_color);
+    update: function(backgroundColor, textColor) {
+        let ratio = this._calculateContrastRatio(backgroundColor, textColor);
         let score = this._scoreFromRatio(ratio);
 
-        this._ratio_label.set_text(ratio.toString());
-        this._score_label.set_text(score);
+        this._ratioLabel.set_text(ratio.toString());
+        this._scoreLabel.set_text(score);
 
-        this.actor.set_style('background-color: ' + background_color + '; color: ' + text_color + ';');
+        this.actor.set_style('background-color: ' + backgroundColor + '; color: ' + textColor + ';');
     },
 });
 
 const ContrastRatioPanel = new Lang.Class({
     Name: 'ContrastRatioPanel',
     Extends: PanelMenu.Button,
-    _background_color_input: null,
-    _text_color_input: null,
-    _contrast_ratio: null,
+    _backgroundColorInput: null,
+    _textColorInput: null,
+    _contrastRatio: null,
 
     _init: function() {
         this.parent(0.0, "Contrast Ratio", false);
@@ -189,40 +189,40 @@ const ContrastRatioPanel = new Lang.Class({
         });
         this.actor.add_actor(icon);
 
-        let background_color_label = new PopupMenu.PopupMenuItem('Background color', { reactive: false });
-        this.menu.addMenuItem(background_color_label);
-        this._background_color_input = new ColorInput('background', '#FFFFFF', this);
-        this.menu.addMenuItem(this._background_color_input);
+        let backgroundColorLabel = new PopupMenu.PopupMenuItem('Background color', { reactive: false });
+        this.menu.addMenuItem(backgroundColorLabel);
+        this._backgroundColorInput = new ColorInput('background', '#FFFFFF', this);
+        this.menu.addMenuItem(this._backgroundColorInput);
 
-        let text_color_label = new PopupMenu.PopupMenuItem('Text color', { reactive: false });
-        this.menu.addMenuItem(text_color_label);
-        this._text_color_input = new ColorInput('text', '#000000', this);
-        this.menu.addMenuItem(this._text_color_input);
-
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-
-        this._contrast_ratio = new ContrastRatio('#FFFFFF', '#000000');
-        this.menu.addMenuItem(this._contrast_ratio);
+        let textColorLabel = new PopupMenu.PopupMenuItem('Text color', { reactive: false });
+        this.menu.addMenuItem(textColorLabel);
+        this._textColorInput = new ColorInput('text', '#000000', this);
+        this.menu.addMenuItem(this._textColorInput);
 
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
-        let swap_colors_button = new PopupMenu.PopupMenuItem('Swap colors', { activate: false });
-        swap_colors_button.actor.connect('button-release-event', Lang.bind(this, this._onSwapColors));
-        this.menu.addMenuItem(swap_colors_button);
+        this._contrastRatio = new ContrastRatio('#FFFFFF', '#000000');
+        this.menu.addMenuItem(this._contrastRatio);
+
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+        let swapColorsButton = new PopupMenu.PopupMenuItem('Swap colors', { activate: false });
+        swapColorsButton.actor.connect('button-release-event', Lang.bind(this, this._onSwapColors));
+        this.menu.addMenuItem(swapColorsButton);
     },
 
     _onSwapColors: function() {
-        let background_color = this._background_color_input.color;
-        let text_color = this._text_color_input.color;
-        this._background_color_input.update(text_color);
-        this._text_color_input.update(background_color);
+        let backgroundColor = this._backgroundColorInput.color;
+        let textColor = this._textColorInput.color;
+        this._backgroundColorInput.update(textColor);
+        this._textColorInput.update(backgroundColor);
         this.updateContrastRatio();
     },
 
     updateContrastRatio: function() {
-        let background_color = this._background_color_input.color;
-        let text_color = this._text_color_input.color;
-        this._contrast_ratio.update(background_color, text_color);
+        let backgroundColor = this._backgroundColorInput.color;
+        let textColor = this._textColorInput.color;
+        this._contrastRatio.update(backgroundColor, textColor);
     },
 });
 
@@ -230,15 +230,15 @@ function init() {
 }
 
 function enable() {
-    contrast_ratio_panel = new ContrastRatioPanel();
-    Main.panel.addToStatusArea("contrast_ratio", contrast_ratio_panel, 0, "right");
+    contrastRatioPanel = new ContrastRatioPanel();
+    Main.panel.addToStatusArea("contrast_ratio", contrastRatioPanel, 0, "right");
 }
 
 function disable() {
-    if (contrast_ratio_panel) {
-        Main.panel._rightBox.remove_actor(contrast_ratio_panel.container);
-        Main.panel.menuManager.removeMenu(contrast_ratio_panel.menu);
-        contrast_ratio_panel.destroy();
-        contrast_ratio_panel = null;
+    if (contrastRatioPanel) {
+        Main.panel._rightBox.remove_actor(contrastRatioPanel.container);
+        Main.panel.menuManager.removeMenu(contrastRatioPanel.menu);
+        contrastRatioPanel.destroy();
+        contrastRatioPanel = null;
     };
 }
