33c1f0fe995f85d07f487ec0ae5fa94a5c8a0410
Larissa Reis <larissa@colivre.coop.br>
2018-05-14 22:15:36 -0600

    Add copyright to color.js

--- color.js
+++ color.js
@@ -1,3 +1,21 @@
+/*
+This file is part of 'contrast-ratio-extension'.
+
+'contrast-ratio-extension' is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License (version 3) as
+published by the Free Software Foundation
+
+'contrast-ratio-extension' is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+'contrast-ratio-extension'.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (c) 2018 Larissa Reis <reiss.larissa@gmail.com>
+*/
+
 const Color = {
     luminance: function(color) {
         let [r, g, b] = this._breakColorComponents(color);
